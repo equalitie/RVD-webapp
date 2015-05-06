6287bfdcb7869089e409a6b0f403fef472c3d153
Bill <bill.doran@gmail.com>
2015-05-06 15:27:10 -0400

    adding basic logic to upload files and store them to path; next pass the file blob to the data processor

--- rvd/views/documents.py
+++ rvd/views/documents.py
@@ -1,8 +1,31 @@
 from flask import Blueprint, render_template
 from flask_login import login_required
+from werkzeug import secure_filename
+
 documents_bp = Blueprint('documents', __name__)
+
+
+UPLOAD_FOLDER = '/path/to/the/uploads'
+ALLOWED_EXTENSIONS = set(['docx', 'doc', 'xlsx', 'xls'])
 
 @documents_bp.route('/document/add', methods=('GET', 'POST'))
 @login_required
 def documents_add():
     return render_template("document_add.html")
+
+@documents_bp.route('/document/upload', methods=('POST'))
+@login_required
+def documents_uploads():
+    uploaded_files = flask.request.files.getlist("file[]") 
+
+    if uploaded_files:
+        for file in uploaded_files:
+            if allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(UPLOAD_FOLDER, filename))
+    return render_template("document_add.html", 'success')
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
