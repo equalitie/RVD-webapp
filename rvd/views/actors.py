75e1f7f63bc28196cb2db24e951dfc37e43f8755
Laurier Rochon <l@laurierrochon.com>
2015-05-11 19:28:20 -0400

    only show events and actors for the owner who entered them (tied to currently logged in user). Needs to rebuild DB for this to work as there are new fields and references.

--- rvd/views/actors.py
+++ rvd/views/actors.py
@@ -5,8 +5,12 @@ from flask_admin import helpers
 from collections import defaultdict
 from rvd.models import session, Actor
 import datetime
+from flask_login import current_user
 from rvd.forms import organisation_factory, location_factory, profession_factory
 from copy import copy
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy import and_
+
 names = {
     'name': 'actor',
     'plural': 'actors',
@@ -33,6 +37,7 @@ def gather_form_data():
     actor_dict['gender'] = actor_dict['gender']
     actor_dict['is_activist'] = actor_dict['is_activist']
     actor_dict['birth_date'] = datetime.datetime.strptime(actor_dict['birth_date'], "%Y-%m-%d").date()
+    actor_dict['owner_id'] = current_user.id
 
     actor_instance = Actor(**actor_dict)
     session.add(actor_instance)
@@ -61,16 +66,25 @@ def flatten_instance(obj):
 
     for r in inspect(Actor).relationships:
         associated_data = getattr(obj, r.key)
-        fields[r.key] = ", ".join([a.name for a in associated_data]) if associated_data else None
+        try:
+            fields[r.key] = ", ".join([a.name for a in associated_data]) if associated_data else None
+        except TypeError:
+            fields[r.key] = associated_data.email
     return fields
 
 
 @actors_bp.route('/actors/<int:actor_id>')
 @login_required
 def view_actor(actor_id):
-    actor = session.query(Actor).get(actor_id)
-    fields = flatten_instance(actor)
     data = copy(names)
+    try:
+        if current_user.is_admin:
+            actor = session.query(Actor).get(actor_id)
+        else:
+            actor = session.query(Actor).filter(and_(Actor.id == actor_id, Actor.owner_id == current_user.id)).one()
+        fields = flatten_instance(actor)
+    except NoResultFound:
+        return redirect('/actors/all')
     data['data'] = fields
 
     return render_template("item_view_single.html", data=data)
@@ -79,7 +93,10 @@ def view_actor(actor_id):
 @actors_bp.route('/actors/all')
 @login_required
 def view_all_actors():
-    all_actors = session.query(Actor).all()
+    if current_user.is_admin:
+        all_actors = session.query(Actor).all()
+    else:
+        all_actors = session.query(Actor).filter(Actor.owner_id == current_user.id)
     all_actors = [{k: v for k, v in x.__dict__.iteritems() if not k.startswith('_sa_')} for x in all_actors]
     data = copy(names)
     data['data'] = all_actors
@@ -90,7 +107,13 @@ def view_all_actors():
 @actors_bp.route('/actors/<int:actor_id>/delete')
 @login_required
 def delete_actor(actor_id):
-    actor = session.query(Actor).get(actor_id)
+    try:
+        if current_user.is_admin:
+            actor = session.query(Actor).get(actor_id)
+        else:
+            actor = session.query(Actor).filter(and_(Actor.id == actor_id, Actor.owner_id == current_user.id)).one()
+    except NoResultFound:
+        return redirect('/actors/all')
     session.delete(actor)
     session.commit()
     return redirect('/actors/all?success=1')
@@ -99,7 +122,14 @@ def delete_actor(actor_id):
 @actors_bp.route('/actors/<int:actor_id>/edit', methods=('GET', 'POST'))
 @login_required
 def edit_actor(actor_id):
-    actor = session.query(Actor).get(actor_id)
+    try:
+        if current_user.is_admin:
+            actor = session.query(Actor).get(actor_id)
+        else:
+            actor = session.query(Actor).filter(and_(Actor.id == actor_id, Actor.owner_id == current_user.id)).one()
+    except NoResultFound:
+        return redirect('/actors/all')
+
     actor_form = ActorForm(request.form, obj=actor)
 
     if helpers.validate_form_on_submit(actor_form):
