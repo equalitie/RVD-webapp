7131bf0c5da07e7ed923db38d98a467ec048569f
Zack Mullaly <redwire@riseup.net>
2015-05-22 14:28:41 -0230

    test org2 parser

--- tp.py
+++ tp.py
@@ -1,14 +1,18 @@
 from rvd import parsers
 
-d = parsers._parse_org1_docx('/Volumes/untitled/Data\ from\ CHIPress\ and\ CCDHRN/CCDHRN/2014.09\ CCDHRN.docx')
+d = parsers._parse_org2_docx('Organization 2 doc name goes here')
 print 'Parsed REPORT'
 print d['report']
 
 print 'Parsed EVENTS'
 for event in d['events']:
     print 'Actor name = ' + event.victims[0].name
+    print 'Actor org  = ' + event.victims[0].organisations[0].name
     print 'Source name = ' + event.sources[0].name
-    loc = event.locations[0]
-    print 'Location = {0} lat = {1} lon = {2}'.format(loc.name, loc.latitude, loc.longitude)
+    try:
+        loc = event.locations[0]
+        print 'Location = '
+        print loc.name
+    except IndexError: pass
 
 print 'Done'
